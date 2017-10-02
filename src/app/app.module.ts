f898e2ec6551e2e95b4e3c31a6324659bb6c9877
Hugo Rios Corona <hugo.rios.cr@gmail.com>
2017-10-02 09:13:53 -0500

    nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ResultadosGeneralesComponent } from '../app/resultados-generales/resultados-generales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultadosGeneralesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'ResultadosGenerales',
+        component: ResultadosGeneralesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/resultados-generales/resultados-generales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados-generales',
+  templateUrl: './resultados-generales.component.html',
+  styleUrls: ['./resultados-generales.component.css']
+})
+export class ResultadosGeneralesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
